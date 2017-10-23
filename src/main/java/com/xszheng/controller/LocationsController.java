ed2611447686d7493d602ca5ac8d463a8f7ae567
RD\xszheng <xszheng@erongdu.com>
2017-10-23 13:44:51 +0800

    依赖修改

--- src/main/java/com/xszheng/controller/LocationsController.java
+++ src/main/java/com/xszheng/controller/LocationsController.java
@@ -1,6 +1,7 @@
 package com.xszheng.controller;
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,7 @@ import com.xszheng.service.LocationsService;
 @RequestMapping(value="/loc")
 public class LocationsController {
 	
-	private static final Logger log = Logger.getLogger(LocationsController.class);
+	private static final Logger log = LoggerFactory.getLogger(LocationsController.class);
 
 	@Autowired
 	private LocationsService locationsService;
