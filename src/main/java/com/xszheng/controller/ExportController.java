5fb2260849282a9a3622cd3981369a73bd70b73d
xiaowoniu93 <1187637436@qq.com>
2018-07-20 01:18:19 +0800

    导出Excel

--- src/main/java/com/xszheng/controller/ExportController.java
+++ src/main/java/com/xszheng/controller/ExportController.java
@@ -19,6 +19,7 @@ import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.util.CellRangeAddress;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -53,12 +54,21 @@ public class ExportController {
 		headFont.setFontName("宋体");		// 字体名称
 		headRowStyle.setFont(headFont);
 		
+		
 		// 创建表头
+		HSSFCell mergeCell = headRow.createCell(2);
+		mergeCell.setCellValue("总分数");
+		mergeCell.setCellStyle(headRowStyle);
+		
+		headRow = sheet.createRow(1);
 		for (int i = 0; i < headStr.length; i++) {
 			HSSFCell cell = headRow.createCell(i);
 			cell.setCellValue(headStr[i]);
 			cell.setCellStyle(headRowStyle);
 		}
+		sheet.addMergedRegion(new CellRangeAddress(0, 1, 0, 0));
+		sheet.addMergedRegion(new CellRangeAddress(0, 1, 1, 1));
+		sheet.addMergedRegion(new CellRangeAddress(0, 0, 2, 4));
 		
 		String fileName = "成绩单";
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
